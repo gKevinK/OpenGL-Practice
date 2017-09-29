6878706d8d9b94b9f352b6a84c3916676fb4816c
KevinK <gkangkai@gmail.com>
2017-09-29 20:54:54 +0800

    Add: texture 2

--- test/frag.glsl
+++ test/frag.glsl
@@ -10,6 +10,6 @@ uniform sampler2D texture;
 void main()
 {
    // FragColor = vec4(1.0f, 0.5f, 1.0f, 1.0f);
-   FragColor = texture(texture, textureCoord);// * vec4(vertexColor, 1.0);
+   FragColor = texture(texture, textureCoord) * vec4(vertexColor, 1.0);
    // FragColor = ourColor;
 }
