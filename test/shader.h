ce6a155952f17c44a59ad7fbf6c6053c441a30bd
KevinK <gkangkai@gmail.com>
2017-10-13 01:15:16 +0800

    Update: Shader.h

--- test/shader.h
+++ test/shader.h
@@ -4,31 +4,45 @@
 #include <glad/glad.h>
 #include <glm/glm.hpp>
 
-void inline setMatrix4fv(unsigned int program, const char *name, const glm::mat4 & mat)
+void inline setMatrix2fv(unsigned int program, const std::string & name, const glm::mat2 & mat)
 {
-	unsigned int loc = glGetUniformLocation(program, name);
-	glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(mat));
+	glUniformMatrix2fv(glGetUniformLocation(program, name.c_str()), 1, GL_FALSE, &mat[0][0]);
 }
 
-void inline setVec3(unsigned int program, const char *name, float v1, float v2, float v3)
+void inline setMatrix3fv(unsigned int program, const std::string & name, const glm::mat3 & mat)
 {
-	unsigned int loc = glGetUniformLocation(program, name);
-	glUniform3f(loc, v1, v2, v3);
+	glUniformMatrix3fv(glGetUniformLocation(program, name.c_str()), 1, GL_FALSE, &mat[0][0]);
 }
 
-void inline setVec3(unsigned int program, const char *name, const glm::vec3 & vec)
+void inline setMatrix4fv(unsigned int program, const std::string & name, const glm::mat4 & mat)
 {
-	unsigned int loc = glGetUniformLocation(program, name);
-	glUniform3fv(loc, 1, glm::value_ptr(vec));
+	glUniformMatrix4fv(glGetUniformLocation(program, name.c_str()), 1, GL_FALSE, &mat[0][0]);
 }
 
-void setFloat(unsigned int program, const char *name, float value)
+void inline setVec3(unsigned int program, const std::string & name, float v1, float v2, float v3)
 {
-	unsigned int loc = glGetUniformLocation(program, name);
-	glUniform1f(loc, value);
+	glUniform3f(glGetUniformLocation(program, name.c_str()), v1, v2, v3);
 }
 
+void inline setVec3(unsigned int program, const std::string & name, const glm::vec3 & vec)
+{
+	glUniform3fv(glGetUniformLocation(program, name.c_str()), 1, &vec[0]);
+}
 
+void inline setInt(unsigned int program, const std::string & name, int value)
+{
+	glUniform1i(glGetUniformLocation(program, name.c_str()), value);
+}
+
+void inline setFloat(unsigned int program, const std::string & name, float value)
+{
+	glUniform1f(glGetUniformLocation(program, name.c_str()), value);
+}
+
+void inline setBool(unsigned int program, const std::string & name, bool value)
+{
+	glUniform1i(glGetUniformLocation(program, name.c_str()), (int)value);
+}
 
 void checkCompileErrors(GLuint shader, std::string type)
 {
@@ -71,25 +85,39 @@ unsigned int LoadShaderProgram(const char *vertexPath, const char *fragmentPath,
 {
 	std::string vertexCode = loadFile(vertexPath);
 	std::string fragmentCode = loadFile(fragmentPath);
-	const char *pvCode = vertexCode.c_str();
+	const char *pvertCode = vertexCode.c_str();
 	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &pvCode, NULL);
+	glShaderSource(vertexShader, 1, &pvertCode, NULL);
 	glCompileShader(vertexShader);
 	checkCompileErrors(vertexShader, "VERTEX");
-	const char *pfCode = fragmentCode.c_str();
+	const char *pfragCode = fragmentCode.c_str();
 	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &pfCode, NULL);
+	glShaderSource(fragmentShader, 1, &pfragCode, NULL);
 	glCompileShader(fragmentShader);
 	checkCompileErrors(fragmentShader, "FRAGMENT");
+	unsigned int geomShader;
+	if (geometryPath != nullptr) {
+		std::string geometryCode = loadFile(geometryPath);
+		const char *pgeomCode = geometryCode.c_str();
+		geomShader = glCreateShader(GL_GEOMETRY_SHADER);
+		glShaderSource(geomShader, 1, &pgeomCode, NULL);
+		glCompileShader(geomShader);
+		checkCompileErrors(geomShader, "GEOMETRY");
+	}
 	unsigned int shaderProgram = glCreateProgram();
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
+	if (geometryPath != nullptr) {
+		glAttachShader(shaderProgram, geomShader);
+	}
 	glLinkProgram(shaderProgram);
 	checkCompileErrors(shaderProgram, "PROGRAM");
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
+	if (geometryPath != nullptr) {
+		glDeleteShader(geomShader);
+	}
 	return shaderProgram;
 }
 
 #endif // !SHADER_H
-
