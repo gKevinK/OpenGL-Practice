c09ffea6801157d6b5147254dadbfdb39c9e1974
KevinK <gkangkai@gmail.com>
2017-11-13 22:58:18 +0800

    Add B1 project 2

--- B1/main.cpp
+++ B1/main.cpp
@@ -1,12 +1,12 @@
 #include <glad\glad.h>
 #include <GLFW\glfw3.h>
+#include <glm\glm.hpp>
+#include <glm\gtc\matrix_transform.hpp>
 #include <iostream>
 #include "shader.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
-std::string to_string(float input, int precision);
 
 unsigned int ScrWidth = 1000;
 unsigned int ScrHeight = 800;
@@ -32,14 +32,67 @@ int main(int argc, char ** argv)
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    glfwSetCursorPosCallback(window, mouse_callback);
-    glfwSetScrollCallback(window, scroll_callback);
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
     glEnable(GL_DEPTH_TEST);
+
+    float vertices[] = {
+        0.5f, -0.5f, 1.0f, 0.0f,
+        0.5f,  0.5f, 1.0f, 1.0f,
+       -0.5f,  0.5f, 0.0f, 1.0f,
+       -0.5f,  0.5f, 0.0f, 1.0f,
+       -0.5f, -0.5f, 0.0f, 0.0f,
+        0.5f, -0.5f, 1.0f, 0.0f,
+    };
+
+    unsigned int shader = loadShaderProgram("main.vert.glsl", "main.frag.glsl");
+    unsigned VAO, VBO, Texture;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)NULL);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)(2 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    while (!glfwWindowShouldClose(window)) {
+        float currentFrame = (float)glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+        processInput(window);
+        glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glUseProgram(shader);
+        glBindVertexArray(VAO);
+
+        setMat4(shader, "model", glm::translate(glm::mat4(1), glm::vec3(1.0f, 2.0f, 3.0f)));
+        glm::mat4 pers = glm::ortho(0.0f, (float)ScrWidth, 0.0f, (float)ScrHeight);
+        setMat4(shader, "proj", pers);
+        glDrawArrays(GL_TRIANGLES, 0, 18);
+
+        glm::vec3 text_green(0.5f, 0.9f, 0.4f);
+        glm::vec3 text_white(1.0f, 1.0f, 1.0f);
+
+        glEnable(GL_CULL_FACE);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+        glDisable(GL_BLEND);
+        glDisable(GL_CULL_FACE);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    //textr.~TextRenderer();
+    //glDeleteVertexArrays(1, &VAO);
+    //glDeleteBuffers(1, &VBO);
+    glfwTerminate();
+    return 0;
+}
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -52,8 +105,4 @@ void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
-}
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
-{
-    camera.ProcessMouseScroll((float)yoffset);
 }
---
+++ B1/shader.h
@@ -0,0 +1,127 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+void inline setMat2(unsigned int program, const std::string & name, const glm::mat2 & mat)
+{
+    glUniformMatrix2fv(glGetUniformLocation(program, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+
+void inline setMat3(unsigned int program, const std::string & name, const glm::mat3 & mat)
+{
+    glUniformMatrix3fv(glGetUniformLocation(program, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+
+void inline setMat4(unsigned int program, const std::string & name, const glm::mat4 & mat)
+{
+    glUniformMatrix4fv(glGetUniformLocation(program, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+
+void inline setVec3(unsigned int program, const std::string & name, float v1, float v2, float v3)
+{
+    glUniform3f(glGetUniformLocation(program, name.c_str()), v1, v2, v3);
+}
+
+void inline setVec3(unsigned int program, const std::string & name, const glm::vec3 & vec)
+{
+    glUniform3fv(glGetUniformLocation(program, name.c_str()), 1, &vec[0]);
+}
+
+void inline setInt(unsigned int program, const std::string & name, int value)
+{
+    glUniform1i(glGetUniformLocation(program, name.c_str()), value);
+}
+
+void inline setFloat(unsigned int program, const std::string & name, float value)
+{
+    glUniform1f(glGetUniformLocation(program, name.c_str()), value);
+}
+
+void inline setBool(unsigned int program, const std::string & name, bool value)
+{
+    glUniform1i(glGetUniformLocation(program, name.c_str()), (int)value);
+}
+
+void checkCompileErrors(GLuint shader, std::string type)
+{
+    GLint success;
+    GLchar infoLog[1024];
+    if (type != "PROGRAM") {
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+        if (!success) {
+            glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+            std::cout << "ERROR::SHADER_COMPILATION_ERROR : " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+        }
+    } else {
+        glGetProgramiv(shader, GL_LINK_STATUS, &success);
+        if (!success) {
+            glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+            std::cout << "ERROR::PROGRAM_LINKING_ERROR : " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+        }
+    }
+}
+
+std::string loadFile(const char *path)
+{
+    std::ifstream fileStream;
+    std::string code;
+    fileStream.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    try {
+        fileStream.open(path);
+        std::stringstream stream;
+        stream << fileStream.rdbuf();
+        fileStream.close();
+        code = stream.str();
+    } catch (std::ifstream::failure e) {
+        std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+        std::cout << path << std::endl;
+    }
+    return code;
+}
+
+unsigned int loadShaderProgram(const char *vertexPath, const char *fragmentPath, const char *geometryPath = nullptr)
+{
+    std::string vertexCode = loadFile(vertexPath);
+    std::string fragmentCode = loadFile(fragmentPath);
+    const char *pvertCode = vertexCode.c_str();
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &pvertCode, NULL);
+    glCompileShader(vertexShader);
+    checkCompileErrors(vertexShader, "VERTEX");
+    const char *pfragCode = fragmentCode.c_str();
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &pfragCode, NULL);
+    glCompileShader(fragmentShader);
+    checkCompileErrors(fragmentShader, "FRAGMENT");
+    unsigned int geomShader;
+    if (geometryPath != nullptr) {
+        std::string geometryCode = loadFile(geometryPath);
+        const char *pgeomCode = geometryCode.c_str();
+        geomShader = glCreateShader(GL_GEOMETRY_SHADER);
+        glShaderSource(geomShader, 1, &pgeomCode, NULL);
+        glCompileShader(geomShader);
+        checkCompileErrors(geomShader, "GEOMETRY");
+    }
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    if (geometryPath != nullptr) {
+        glAttachShader(shaderProgram, geomShader);
+    }
+    glLinkProgram(shaderProgram);
+    checkCompileErrors(shaderProgram, "PROGRAM");
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    if (geometryPath != nullptr) {
+        glDeleteShader(geomShader);
+    }
+    return shaderProgram;
+}
+
+#endif // !SHADER_H
