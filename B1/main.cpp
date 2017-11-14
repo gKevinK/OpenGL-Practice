5fe5d80d19092383a2379099e9b0f7a12b491643
KevinK <gkangkai@gmail.com>
2017-11-14 11:02:15 +0800

    B1: Nearly completed

--- B1/main.cpp
+++ B1/main.cpp
@@ -2,11 +2,43 @@
 #include <GLFW\glfw3.h>
 #include <glm\glm.hpp>
 #include <glm\gtc\matrix_transform.hpp>
+#include <stb_image.h>
 #include <iostream>
+#include <list>
 #include "shader.h"
+
+class Star
+{
+public:
+    glm::vec3 Dir;
+    glm::vec3 Color;
+    float GenTime;
+    float CurrentTime;
+
+    Star() {}
+
+    Star(glm::vec3 direction, glm::vec3 color, float genTime)
+        : Dir(direction), Color(color), GenTime(genTime) {}
+
+    glm::vec3 Position(int mode = 1)
+    {
+        return Dir * (CurrentTime - GenTime);
+    }
+
+    glm::vec3 GetColor()
+    {
+        float life = CurrentTime - GenTime;
+        if (life > 6.0f)
+            return (8.0f - life) / 2.0f * Color;
+        else
+            return Color;
+    }
+};
+
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
+unsigned int loadTexture(const std::string & path);
 
 unsigned int ScrWidth = 1000;
 unsigned int ScrHeight = 800;
@@ -16,13 +48,15 @@ bool firstMouse = true;
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
+int mode;
+
 int main(int argc, char ** argv)
 {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_SAMPLES, 8);
+    glfwWindowHint(GLFW_SAMPLES, 4);
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     GLFWwindow* window = glfwCreateWindow(ScrWidth, ScrHeight, "C-2", NULL, NULL);
     if (window == NULL) {
@@ -36,7 +70,7 @@ int main(int argc, char ** argv)
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-    glEnable(GL_DEPTH_TEST);
+    //glEnable(GL_DEPTH_TEST);
 
     float vertices[] = {
         0.5f, -0.5f, 1.0f, 0.0f,
@@ -46,6 +80,8 @@ int main(int argc, char ** argv)
        -0.5f, -0.5f, 0.0f, 0.0f,
         0.5f, -0.5f, 1.0f, 0.0f,
     };
+    std::list<Star> stars;
+    stars.push_back(Star(glm::vec3(1.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), 0));
 
     unsigned int shader = loadShaderProgram("main.vert.glsl", "main.frag.glsl");
     unsigned VAO, VBO, Texture;
@@ -58,31 +94,46 @@ int main(int argc, char ** argv)
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)(2 * sizeof(float)));
     glEnableVertexAttribArray(1);
+    Texture = loadTexture("resource\\Star.bmp");
+    glActiveTexture(GL_TEXTURE0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glm::vec3 lastDir = glm::vec3(1.0f, 0.0f, 0.0f);
+    float lastTime = 0;
+    float genDelta = 2.0f / 16;
+    float angleDelta = glm::radians(360.0f / 16);
+
     while (!glfwWindowShouldClose(window)) {
         float currentFrame = (float)glfwGetTime();
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
         processInput(window);
+
+        while (currentFrame - lastTime > genDelta) {
+            lastTime += genDelta;
+            lastDir = glm::mat3(glm::rotate(glm::mat4(1),
+                angleDelta, glm::vec3(0.0f, 0.0f, 1.0f))) * lastDir;
+            stars.push_back(Star(lastDir, glm::vec3(1.0f, 1.0f, 1.0f), lastTime));
+        }
+        while (!stars.empty() && currentFrame - stars.front().GenTime > 8.0) {
+            stars.pop_front();
+        }
+
         glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shader);
         glBindVertexArray(VAO);
+        setMat4(shader, "proj", glm::ortho(-10.0f / ScrHeight * ScrWidth, 10.0f / ScrHeight * ScrWidth, -10.0f, 10.0f));
 
-        setMat4(shader, "model", glm::translate(glm::mat4(1), glm::vec3(1.0f, 2.0f, 3.0f)));
-        glm::mat4 pers = glm::ortho(0.0f, (float)ScrWidth, 0.0f, (float)ScrHeight);
-        setMat4(shader, "proj", pers);
-        glDrawArrays(GL_TRIANGLES, 0, 18);
-
-        glm::vec3 text_green(0.5f, 0.9f, 0.4f);
-        glm::vec3 text_white(1.0f, 1.0f, 1.0f);
-
-        glEnable(GL_CULL_FACE);
         glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-        glDisable(GL_BLEND);
-        glDisable(GL_CULL_FACE);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE);
+        for (Star & s : stars) {
+            s.CurrentTime = currentFrame;
+            setMat4(shader, "model", glm::translate(glm::mat4(1), s.Position()));
+            setVec3(shader, "color", s.GetColor());
+            glDrawArrays(GL_TRIANGLES, 0, 6);
+        }
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -105,4 +156,34 @@ void processInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+}
+
+unsigned int loadTexture(const std::string & path)
+{
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+    int width, height, nrComponents;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char *data = stbi_load(path.c_str(), &width, &height, &nrComponents, 0);
+    if (data) {
+        GLenum format;
+        if (nrComponents == 1)
+            format = GL_RED;
+        else if (nrComponents == 3)
+            format = GL_RGB;
+        else if (nrComponents == 4)
+            format = GL_RGBA;
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        stbi_image_free(data);
+    } else {
+        std::cout << "Texture failed to load at path: " << path << std::endl;
+        stbi_image_free(data);
+    }
+    return textureID;
 }
