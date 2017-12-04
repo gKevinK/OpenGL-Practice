d84e27ee77047ad67da57e635be44afb2f107e22
KevinK <gkangkai@gmail.com>
2017-12-04 22:25:45 +0800

    tmp

--- A6/terrain.cpp
+++ A6/terrain.cpp
@@ -1,2 +1,40 @@
 #include "terrain.hpp"
 
+Terrain::Terrain()
+{
+    for (int i = 0; i < 20; i++) {
+        for (int j = 0; j < 20; j++) {
+            float fi = (float)i;
+            float fj = (float)j;
+            float vs[] = {
+                fi, fj, fi, fj,
+                fi, fj + 1, fi, fj + 1,
+                fi + 1, fj, fi + 1, fj,
+                fi + 1, fj + 1, fi + 1, fj + 1 };
+            for (int k = 0; k < 16; k++) {
+                vertices.push_back(vs[k] / 20.0f);
+            }
+        }
+    }
+}
+
+void Terrain::Init()
+{
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.size(), &vertices[0], GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void *)(2 * sizeof(float)));
+    glBindVertexArray(0);
+}
+
+void Terrain::Draw(unsigned int shader)
+{
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+    glBindVertexArray(0);
+}
--- A6/terrain.hpp
+++ A6/terrain.hpp
@@ -1,1 +1,20 @@
-#pragma once
+#ifndef TERRAIN_H
+#define TERRAIN_H
+
+#include <glad\glad.h>
+#include <vector>
+
+class Terrain
+{
+public:
+    unsigned int VAO, VBO;
+
+    Terrain();
+    void Init();
+    void Draw(unsigned int shader);
+
+private:
+    std::vector<float> vertices;
+};
+#endif // !TERRAIN_H
+
