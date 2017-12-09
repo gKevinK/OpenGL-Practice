583f96a561bb9384be32fce0c4c03848c6e3e2cf
KevinK <gkangkai@gmail.com>
2017-12-09 11:13:20 +0800

    A5: Add: Compute shader 1

--- A5/main.comp.glsl
+++ A5/main.comp.glsl
@@ -3,28 +3,63 @@
 layout(local_size_x = 8, local_size_y = 8) in;
 layout(rgba32f, binding = 0) uniform image2D img;
 
-//uniform image2D img;
+struct Sphere {
+	vec3 center;
+	vec3 color;
+	float radius;
+	float eta1;
+	float eta2;
+};
+
+struct Triangle
+{
+    vec3 v1;
+    vec3 v2;
+    vec3 v3;
+    vec3 color;
+    float eta1;
+    float eta2;
+};
+
+struct Ray {
+	vec3 origin;
+	vec3 dir;
+};
+
 uniform sampler2D spheres;
+uniform sampler2D triangles;
+
+uniform int width;
+uniform int height;
+uniform vec3 viewPos;
+uniform vec3 r00;
+uniform vec3 r01;
+uniform vec3 r10;
+uniform vec3 r11;
+
+bool hitSphere(Sphere sphere, Ray ray);
+bool hitTriangle(Triangle triangle, Ray ray);
 
 void main() {
 	vec4 pixel = vec4(1.0, 0.0, 0.0, 1.0);
 	ivec2 coord = ivec2(gl_GlobalInvocationID.xy);
-    //ivec2 localCoord = ivec2(gl_LocalInvocationID.xy);
+    ivec2 localCoord = ivec2(gl_LocalInvocationID.xy);
 
-	int i = 1, j = 0;
-	while (true) {
-		if (j == 0) {
-			i++;
-			if (i == 10) {
-				j = 1;
-			}
-		} else if (i > 0)
-			i--;
-		else
-			break;
-	}
-    pixel.y += j;
+	if (length(coord - ivec2(width, height) / 2) < 100)
+		pixel = vec4(1.0, 1.0, 0.0, 1.0);
+	if (length(coord - ivec2(width, height) / 2) < 25)
+		pixel = vec4(1.0, 1.0, 1.0, 1.0);
 
 	imageStore(img, coord, pixel);
+}
+
+bool hitSphere(Sphere sphere, Ray ray)
+{
+	return false;
+}
+
+bool hitTriangle(Triangle triangle, Ray ray)
+{
+	return false;
 }
 
