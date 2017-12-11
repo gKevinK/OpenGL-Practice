219066037ab0a77158b54c8eb8171fbe0bd42995
KevinK <gkangkai@gmail.com>
2017-12-11 12:16:17 +0800

    A5: Add: HitSphere

--- A5/main.comp.glsl
+++ A5/main.comp.glsl
@@ -6,11 +6,11 @@ layout(rgba32f, binding = 0) uniform image2D img;
 #define EPSILON 0.00001
 
 struct Sphere {
-	vec3 center;
-	vec3 color;
-	float radius;
-	float eta1;
-	float eta2;
+    vec3 center;
+    vec3 color;
+    float radius;
+    float eta1;
+    float eta2;
 };
 
 struct Triangle
@@ -24,8 +24,8 @@ struct Triangle
 };
 
 struct Ray {
-	vec3 origin;
-	vec3 dir;
+    vec3 origin;
+    vec3 dir;
 };
 
 struct StackFrame {
@@ -49,52 +49,45 @@ uniform vec3 r11;
 bool hitSphere(Sphere sphere, Ray ray);
 bool hitTriangle(Triangle triang, Ray ray);
 
-int value(int a)
-{
-	if (a == 0) return 0;
-	return 1 + value(a - 1);
-}
-
-StackFrame frames[16];
+Sphere sph = Sphere(vec3(0.0), vec3(1.0), 1.0, 1.0, 1.0);
 
 void main()
 {
-	vec4 pixel = vec4(0.1, 0.1, 0.1, 1.0);
-	ivec2 p = ivec2(gl_GlobalInvocationID.xy);
+    vec4 pixel = vec4(0.1, 0.1, 0.1, 1.0);
+    ivec2 p = ivec2(gl_GlobalInvocationID.xy);
     vec2 up = vec2(p) / vec2(width, height);
     //ivec2 lp = ivec2(gl_LocalInvocationID.xy);
-
+    StackFrame frames[16];
+    
     Ray ray;
     ray.origin = viewPos;
-    ray.dir = mix(mix(r00, r01, up.x), mix(r10, r11, up.x), up.y);
+    ray.dir = normalize(mix(mix(r00, r01, up.x), mix(r10, r11, up.x), up.y));
 
-	int i = 1;
-	while (true) {
-		if (i == 0) break;
-		i--;
-	}
 
-	imageStore(img, p, pixel);
+    if (hitSphere(sph, ray))
+        pixel = vec4(1.0, 0.0, 0.0, 1.0);
+
+    imageStore(img, p, pixel);
 }
 
 bool hitSphere(Sphere s, Ray r)
 {
-/* vec2 intersectSphere(vec3 origin, vec3 dir, const sphere s) {
-	vec3 L = s.center - r.origin;
-	float tca = dot(L, r.dir);
-	float d2 = dot(L, L) - tca * tca;
-	if (d2 > s.radius * s.radius)
-		return vec2(-1.0);
-	float thc = sqrt(s.r * s.r - d2);
-	float t0 = tca - thc;
-	float t1 = tca + thc;
-	if (t0 < t1 && t1 >= 0.0)
-		return vec2(t0, t1); */
-	return false;
+    vec3 L = s.center - r.origin;
+    float tca = dot(L, r.dir);
+    float d2 = dot(L, L) - tca * tca;
+    if (d2 > s.radius * s.radius)
+        return false;
+    float thc = sqrt(s.radius * s.radius - d2);
+    float t0 = tca - thc;
+    float t1 = tca + thc;
+//    if (t0 < t1 && t1 >= 0.0)
+//        return vec2(t0, t1);
+    return true;
 }
 
 bool hitTriangle(Triangle triang, Ray ray)
 {
-	return false;
+    // TODO
+    return false;
 }
 
